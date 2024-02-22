bebb56c17a4c9b487c1093a107f6d5e5b9b6b989
clearup23 <154214761+clearup23@users.noreply.github.com>
2024-02-22 16:32:38 +0530

    Update FullstackBackendApplication.java

--- src/main/java/com/example/demo/FullstackBackendApplication.java
+++ src/main/java/com/example/demo/FullstackBackendApplication.java
@@ -22,7 +22,7 @@ public class FullstackBackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://50.16.70.47:3000")
+						.allowedOrigins("http://3.108.220.24:3000")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*");
 			}
